500d4d485b40b39081913d3ec0ef61e470cf45f3
Stelios <steliosain@hotmail.com>
2025-12-02 17:45:26 +0200

    added create client logic and database tables

--- src/main/java/com/stelios/bankmanagementsystem/Controllers/Admin/CreateClientController.java
+++ src/main/java/com/stelios/bankmanagementsystem/Controllers/Admin/CreateClientController.java
@@ -1,5 +1,6 @@
 package com.stelios.bankmanagementsystem.Controllers.Admin;
 
+import com.stelios.bankmanagementsystem.Models.Model;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -8,6 +9,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 public class CreateClientController implements Initializable {
@@ -23,8 +26,68 @@ public class CreateClientController implements Initializable {
     public Button create_client_btn;
     public Text error_lbl;
 
+
+    private String payeeAddress;
+    private boolean createCheckingsAccountFlag = false;
+    private boolean createSavingsAccountFlag = false;
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+
     }
+
+    private void createAccount(String accountType) {
+        double balance = Double.parseDouble(ch_amount_fld.getText());
+
+        //Create account number
+        String firstSection = "0429";
+        String lastSection = Integer.toString((new Random()).nextInt(9999)+1000);
+        String accountNumber = firstSection + " " + lastSection;
+        if (accountType.equals("Checking")) {
+            Model.getInstance().getDatabaseDriver().createCheckingAccount(payeeAddress, accountNumber, 10, balance);
+        }else if (accountType.equals("Savings")) {
+            Model.getInstance().getDatabaseDriver().createSavingsAccount(payeeAddress, accountNumber, 2000, balance);
+        }
+    }
+
+    private void createClient() {
+        if (createCheckingsAccountFlag) {
+            createAccount("Checking");
+        }
+
+        if (createSavingsAccountFlag) {
+            createAccount("Savings");
+        }
+
+        String fName = fName_fld.getText();
+        String lName = lName_fld.getText();
+        String password = password_fld.getText();
+        Model.getInstance().getDatabaseDriver().createClient(fName, lName, payeeAddress, password, LocalDate.now());
+        error_lbl.setStyle("-fx-text-fill: green;  -fx-font-size: 1.3em; -fx-font-weight: bold;");
+        error_lbl.setText("Client Account created successfully!");
+        resetFields();
+    }
+
+
+    private void resetFields() {
+        fName_fld.clear();
+        lName_fld.clear();
+        password_fld.clear();
+        pAddress_box.setSelected(false);
+        pAddress_lbl.setText("");
+        ch_acc_box.setSelected(false);
+        ch_amount_fld.clear();
+        sv_acc_box.setSelected(false);
+        sv_acc_fld.clear();
+    }
+
+
+
+
+
+
+
+
 }
--- src/main/java/com/stelios/bankmanagementsystem/Models/DatabaseDriver.java
+++ src/main/java/com/stelios/bankmanagementsystem/Models/DatabaseDriver.java
@@ -1,6 +1,7 @@
 package com.stelios.bankmanagementsystem.Models;
 
 import java.sql.*;
+import java.time.LocalDate;
 
 public class DatabaseDriver {
     private Connection connection;
@@ -46,6 +47,42 @@ public class DatabaseDriver {
             e.printStackTrace();
         }
         return resultSet;
+    }
+
+    public void createClient(String fName, String lName, String pAddress, String password, LocalDate date){
+        Statement statement;
+        try{
+            statement = this.connection.createStatement();
+            statement.executeUpdate("INSERT INTO " +
+                    "Clients(FirstName, LastName, PayeeAddress, Password, Date)"+
+                    "VALUES ('"+fName+"', '"+lName+"', '"+pAddress+"', '"+password+"', '"+date.toString()+"');");
+        }catch(SQLException e){
+            System.err.println("DATABASE ERROR executing createClient query.");
+        }
+    }
+
+    public void createCheckingAccount(String owner, String number, double tLimit, double balance){
+        Statement statement;
+        try{
+            statement = this.connection.createStatement();
+            statement.executeUpdate("INSERT INTO " +
+                    "CheckingsAccounts(Onwer, AccountNumber, TransactionLimit, Balance)"+
+                    "VALUES ('"+owner+"', '"+number+"', '"+tLimit+"', '"+balance+"');");
+        }catch(SQLException e){
+            System.err.println("DATABASE ERROR executing createCheckingAccount query.");
+        }
+    }
+
+    public void createSavingsAccount(String owner, String number, double wLimit, double balance){
+        Statement statement;
+        try{
+            statement = this.connection.createStatement();
+            statement.executeUpdate("INSERT INTO " +
+                    "SavingsAccounts(Onwer, AccountNumber, WithdrawalLimit, Balance)"+
+                    "VALUES ('"+owner+"', '"+number+"', '"+wLimit+"', '"+balance+"');");
+        }catch(SQLException e){
+            System.err.println("DATABASE ERROR executing createSavingsAccount query.");
+        }
     }
 
 
